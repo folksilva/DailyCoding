3a9d88ab3ec9e72f168a353503da3659543be90e
Luiz Silva <lfsilva@sccorinthians.com.br>
2018-02-05 15:26:19 -0200

    Adiciona o problema 35, sem solução

---
+++ problem35.py
@@ -0,0 +1,23 @@
+"""
+This problem was asked by Google.
+
+Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the array so 
+that all the Rs come first, the Gs come second, and the Bs come last. You can only swap elements of 
+the array.
+
+Do this in linear time and in-place.
+
+For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become 
+['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+
+"""
+
+
+def problem35(input):
+    pass
+
+
+def test_problem35():
+    input = ['G', 'B', 'R', 'R', 'B', 'R', 'G']
+    output = ['R', 'R', 'R', 'G', 'G', 'B', 'B']
+    assert problem35(input) == output
